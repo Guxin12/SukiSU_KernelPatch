2266bda866d23c02cd8ba661b2ae93c8170df36f
Guxin12 <2452148391@qq.com>
2026-07-08 10:28:08 +0800

    fix build[1]
    Signed-off-by: Guxin12 <2452148391@qq.com>

--- kernel/base/start.c
+++ kernel/base/start.c
@@ -2,15 +2,15 @@
 /* 
  * Copyright (C) 2023 bmax121. All Rights Reserved.
  */
-
+#include <ktypes.h>
+#include <predata.h>
+#include <preset.h>
 #include <common.h>
 #include <pgtable.h>
-#include <ktypes.h>
 #include <kallsyms.h>
 #include <compiler.h>
 #include <cache.h>
 #include <symbol.h>
-#include <predata.h>
 #include <barrier.h>
 #include <stdarg.h>
 
